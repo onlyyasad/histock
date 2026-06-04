8433f4ddaee9327052a1ddeed921707ee8081cec
hafij <hafij.alasad23@gmail.com>
2026-06-05 02:53:03 +0600

    fix(sellers): add tooltipprovider required by shadcn sidebar

--- apps/sellers/src/app/providers.tsx
+++ apps/sellers/src/app/providers.tsx
@@ -3,12 +3,15 @@
 import { Provider } from 'react-redux'
 import { Toaster } from 'sonner'
 import { store } from '@/store/store'
+import { TooltipProvider } from '@/components/ui/tooltip'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      {children}
-      <Toaster position="top-right" richColors />
+      <TooltipProvider>
+        {children}
+        <Toaster position="top-right" richColors />
+      </TooltipProvider>
     </Provider>
   )
 }
